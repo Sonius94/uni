e6e5d6b035271a56ba67a84d1939e91524fde3e0
Tom Kastek <tomkastek@gmail.com>
2018-01-23 21:44:21 +0100

    brute force initialised

---
+++ GWV/Blatt11/Willikins-Seating-Problem/src/Relation.java
@@ -0,0 +1,12 @@
+
+public class Relation {
+	String _personA;
+	String _personB;
+	int _rating;
+	
+	public Relation(String personA, String personB, int rating) {
+		_personA = personA;
+		_personB = personB;
+		_rating = rating;
+	}
+}
---
+++ GWV/Blatt11/Willikins-Seating-Problem/src/Willikins.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Willikins {
+	public static void main(String[] args) {
+		Willikins wilikins = new Willikins();
+		ArrayList<String> persons = wilikins.generateFixedPersonList();
+		Hashtable<String,Integer> relations = wilikins.generateFixedRatingList(persons);
+		wilikins.solveBruteForce(persons,relations);
+	}
+	
+	public void solveBruteForce(ArrayList<String> persons, Hashtable<String,Integer> relations) {
+		ArrayList<String> seatOrder = persons;
+		ArrayList<ArrayList<String>> permutations = generatePerm(persons);
+		int totalRating = permutations.size()*(-4);
+		for(int i = 0; i<permutations.size();i++) {
+			int rating = getRating(permutations.get(i),relations);
+			System.out.println(permutations.get(i));
+			System.out.println(rating);
+			if (rating > totalRating) {
+				totalRating = rating;
+				seatOrder = permutations.get(i);
+			}
+		}
+		System.out.println("Optimum:");
+		System.out.println(seatOrder);
+		System.out.println(totalRating);
+		// Whatto do if relation not found
+	}
+	
+	public void solveAndGenerate(ArrayList<String> persons) {
+		// generiere Relations
+		// Whatto do if relation not found
+	}
+	
+	public void solveWithSavedRelations(ArrayList<String> persons) {
+		// Nimm die gespeicherte
+		// Whatto do if relation not found
+	}
+	
+	public ArrayList<String> generateFixedPersonList() {
+		ArrayList<String> persons = new ArrayList<String>();
+		persons.add("Emil");
+		persons.add("Anton");
+		persons.add("Hans");
+		persons.add("Kerstin");
+		return persons;
+	}
+	/*
+	public Hashtable<String,Integer> generateFixedRatingList() {
+		Hashtable<String,Integer> ht = new Hashtable<String,Integer>();
+		ht.put("EmilAnton", 4);
+		ht.put("AntonEmil", 4);
+		ht.put("EmilHans", 3);
+		ht.put("HansEmil", 3);
+		ht.put("AntonHans", -2);
+		ht.put("HansAnton", -2);
+		
+		ht.put("KerstinAnton", -4);
+		ht.put("AntonKerstin", -4);
+		ht.put("EmilKerstin", 0);
+		ht.put("KerstinEmil", 0);
+		ht.put("HansKerstin", 1);
+		ht.put("KerstinHans", 1);
+		return ht;
+	}*/
+	
+	public Hashtable<String,Integer> generateFixedRatingList(ArrayList<String> names) {
+		Hashtable<String,Integer> ratings = new Hashtable<String,Integer>();
+		for (String name : names) {
+			for (String secondName : names) {
+				String combName = name + secondName;
+				String combNameReverse = secondName + name;
+				if (ratings.get(combName) == null)  {
+					int randomNum = ThreadLocalRandom.current().nextInt(-4,5);
+					ratings.put(combName, randomNum);
+					ratings.put(combNameReverse, randomNum);
+				}
+			}
+		}
+		System.out.println(ratings.toString());
+		return ratings;
+	}
+	
+	
+	
+	public int getRating(ArrayList<String> seatOrder,Hashtable<String,Integer> ratings) {
+		int rating = 0;
+		for(int i = 0; i<seatOrder.size();i++) {
+			String personA = seatOrder.get(i);
+			String personB;
+			if(i < seatOrder.size() - 1) {
+				personB = seatOrder.get(i+1);
+			} else {
+				personB = seatOrder.get(0);
+			}
+			String fullName = personA + personB;
+			rating += ratings.get(fullName);
+		}
+		return rating;
+	}
+	
+	 public ArrayList<ArrayList<String>> generatePerm(ArrayList<String> original) {
+	      if (original.size() == 0) { 
+	    	  ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+	          result.add(new ArrayList<String>());
+	          return result;
+	      }
+
+	      String firstElement = original.remove(0);
+	      ArrayList<ArrayList<String>> returnValue = new ArrayList<ArrayList<String>>();
+	      ArrayList<ArrayList<String>> permutations = generatePerm(original);
+
+	      for (ArrayList<String> smallerPermutated : permutations) {
+	          for (int index=0; index <= smallerPermutated.size(); index++) {
+	        	  ArrayList<String> temp = new ArrayList<String>(smallerPermutated);
+	              temp.add(index, firstElement);
+	              returnValue.add(temp);
+	          }
+	      }
+
+	      return returnValue;
+	  }
+}
